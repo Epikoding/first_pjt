9ceddacce0596104883d7a95723bcf59f3cacc95
Doyup Kim <gltlvl12@gmail.com>
2022-04-10 18:03:20 +0900

    비밀번호 암호화, jwt, time

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-app = Flask(__name__)
+import hashlib
 
+import jwt as jwt
 from pymongo import MongoClient
 
-import hashlib
-import jwt
-
-from datatime import datatime, timedelta
 
 import certifi
 ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.kxazb.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# 비밀 키 설정
+SECRET_KEY = 'SPARTA'
 
+# 홈 페이지
 @app.route('/')
 def home():
     return render_template('login.html')
 
-
+# 회원 가입 페이지 이동
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-
+# 회원 가입
 @app.route("/users_signup", methods=["POST"])
 def users():
     id_receive = request.form['id_give']
@@ -44,12 +46,28 @@ def users():
     return jsonify({'msg':'회원 가입 완료!'})
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    if request.method == 'POST':
-        _Id_ = request.form['_ID_']
-        _Pw_ = request.form['_PW_']
-        user = db.users.find.one({'user_id' : _Id_},{'user_pwd':_Pw_})
+@app.route('/sign_in', methods=['POST'])  # 로그인 API
+def sign_in():
+    id_receive = request.form['give_id']
+    pw_receive = request.form['give_pw']
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()  # 패스워드 암호화
+
+    result = db.users.find_one({'id': id_receive, 'pw': pw_hash})  # 동일한 유저가 있는지 확인
+
+    if result is not None:  # 동일한 유저가 없는게 아니면, = 동일한 유저가 있으면,
+        payload = {
+            'id': id_receive,
+
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf8')
+            # .decode('utf8')  # 토큰을 건내줌.
+
+
+        return jsonify({'result': 'success', 'token': token})
+    else:  # 동일한 유저가 없으면,
+        return jsonify({'result': 'fail', 'msg': '아이디/패스워드가 일치하지 않습니다.'})
 
 
 
